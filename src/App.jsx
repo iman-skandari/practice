6a9730eaf1cdc85c6f4d97446e2eb91ff5ac548c
iman-skandari <iimisk9919@gmail.com>
2023-06-30 11:37:41 +0330

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Navbar from'./Navbar'
+import './App.css'
+
+import React from 'react'
+var data ='sdsd'
+const App = () => {
+    const showSidebar = ()=> {
+      sdsdsd
+    }
+  return (
+    <div>
+      <Navbar/>
+      <div className='landingWrapper'> 
+        <p></p>
+      </div>
+      <div className='sdsds' onClick={showSidebar}>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
+
